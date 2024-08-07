f3bb9e31e091dcebb205c7c2d3b38eb6aee32d9c
lumawewe <137160002+lumawewe@users.noreply.github.com>
2024-08-07 11:45:47 -0300

    Update Message.js

--- models/Message.js
+++ models/Message.js
@@ -1,46 +1,44 @@
 import { Schema, model, models } from 'mongoose';
 
-const MessageSchema = new Schema(
+const UserSchema = new Schema(
   {
-    sender: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    recipient: {
-      type: Schema.Types.ObjectId,
-      ref: 'User',
-      required: true,
-    },
-    property: {
-      type: Schema.Types.ObjectId,
-      ref: 'Property',
-      required: true,
-    },
-    name: {
-      type: String,
-      required: [true, 'Name is required'],
-    },
     email: {
       type: String,
-      required: [true, 'Email is required'],
+      unique: [true, 'It looks like this email is already in use. You might already be a member of Feels!'], // Email uniqueness
+      required: [true, 'We need your email to proceed. Please provide it.'],  // Email required
+      trim: true,                            // Removes extra spaces
+      lowercase: true,                       // Converts email to lowercase
     },
-    phone: {
+    username: {
       type: String,
+      required: [true, 'A username is needed to set up your account. Please choose one.'], // Username required
+      trim: true,                             // Removes extra spaces
+      unique: [true, 'This username is already taken. Please choose another one.'], // Username uniqueness
+      minlength: [3, 'Your username should be at least 3 characters long. Try a longer one.'], // Minimum length
+      maxlength: [30, 'Username can’t be longer than 30 characters. Please shorten it.'], // Maximum length
     },
-    body: {
+    image: {
       type: String,
+      default: 'default-profile.png', // Default image
+      validate: {
+        validator: function(v) {
+          return /^(https?:\/\/.*\.(?:png|jpg|jpeg|gif))$/.test(v); // Image URL format validation
+        },
+        message: 'The image URL seems to be invalid. Please provide a valid URL in PNG, JPG, JPEG, or GIF format.', // URL format
+      },
     },
-    read: {
-      type: Boolean,
-      default: false,
-    },
+    bookmarks: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: 'Property', // References the Property model
+      },
+    ],
   },
   {
-    timestamps: true,
+    timestamps: true, // Automatically adds createdAt and updatedAt fields
   }
 );
 
-const Message = models.Message || model('Message', MessageSchema);
+const User = models.User || model('User', UserSchema);
 
-export default Message;
+export default User;
